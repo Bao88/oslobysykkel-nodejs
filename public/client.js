3cb3a996c60f454d9951b6f92f85a8b35bf3242b
HBN <ngohoangbao@outlook.com>
2018-03-12 17:29:48 +0100

    local

--- public/client.js
+++ public/client.js
@@ -1,26 +1,40 @@
-// client-side js
-// run by the browser each time your view template is loaded
+// window.onload = function(){
+//     fetch('http://example.com/movies.json')
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       console.log(myJson);
+//     });
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
+// }
 
-$(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
+function buttonPressed(event){
+  alert("https://oslobysykkel.no/api/v1/"+event.classList[0]);
+  // let xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = function() {
+  //     if (this.readyState == 4 && this.status == 200) {
+  //        console.log(this.responseText);
+  //     }
+  // };
+  // xhttp.open("GET", "https://oslobysykkel.no/api/v1/stations", true);
+  // xhttp.setRequestHeader("Client-Identifier", "aa57ec526474715c30fdc77f2a7da0c0");
+  // xhttp.send();
+
+    
+  fetch('https://oslobysykkel.no/api/v1/' + event.classList[0], {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+          "Client-Identifier": "aa57ec526474715c30fdc77f2a7da0c0",
+      }
+      
+  }).then(function(response){
+      if(response.ok) return response.json();
+      throw new Error("Couldn't fetch data from API!");
+  }).then(function(myJson) {
+      console.log(myJson);
+  }).catch(function(error){
+      console.log('There has been a problem with your fetch operation: ', error.message);
   });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
-
-});
+}
