d6c3889c7647675dbc435087fac2c3b275819f12
Glitch (oslobysykkel-nodejs) <none>
2018-03-12 17:02:57 +0000

    😥🏏 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,5 +1,20 @@
 function buttonPressed(event){
-  alert("https://oslobysykkel.no/api/v1/"+event.classList[0]);
-
+  alert("https://oslobysykkel.no/api/v1/"+event.innerHTML.toLowerCase());
+  let xmlhttp;
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    // code for older browsers
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML =
+      this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "https://oslobysykkel.no/api/v1/"+event.innerHTML.toLowerCase(), true);
+  xmlhttp.send();
 }
 
